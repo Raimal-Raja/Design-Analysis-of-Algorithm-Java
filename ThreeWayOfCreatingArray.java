d0252a7e2fbc78d2d9b5b021ebc34c4887a5f63e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-21 22:25:19 +0500

    updated

---
+++ ThreeWayOfCreatingArray.java
@@ -0,0 +1,16 @@
+public class ThreeWayOfCreatingArray {
+    public static void main(String[]args){
+        //Method 1
+        int[] arry = new int[5];
+
+        //Method 2
+        int[]arry2;
+        arry2 = new int[5];
+
+        //Method 3
+        int[] arry3 = {2,4,6,8,10};
+
+        //Method 4
+        int arry4 [] = {1,2,3,45,5};
+    }
+}
