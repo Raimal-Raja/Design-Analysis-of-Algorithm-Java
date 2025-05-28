eeb4e2b79b7d12360d5ccfaedbfe8b2b4389c813
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-05-28 09:10:26 +0500

    Updated

--- AddArrayUsingMethod.java
+++ AddArrayUsingMethod.java
@@ -1,12 +1,31 @@
-import java.util.Arrays;
+import java.util.*;
 public class AddArrayUsingMethod{
     public static void main(String[]args){
 
-        int[]arr = {2,3,4,5,6,7};
+        int[]arr = {2,3,4,5,4,7};
         System.out.println(Arrays.toString(arr));
 
         int sum = add(arr);
         System.out.println("the sum of array is: " +sum);
+        System.out.println(mostFreq(arr));
+    }
+
+    public static int mostFreq(int arr[]){
+        Map <Integer,Integer> freqMap=new HashMap();
+        int mostFreqEle=0;
+        int maxFreq=0;
+        for(int i=0;i<arr.length;i++){
+            freqMap.put(arr[i],freqMap.getOrDefault(arr[i], 0)+1);
+            if(freqMap.get(arr[i])>maxFreq){
+                mostFreqEle=arr[i];
+                maxFreq=freqMap.get(arr[i]);
+            }
+
+        }
+        
+            
+        
+        return mostFreqEle;
     }
 
 private static int add(int[] arr2){
